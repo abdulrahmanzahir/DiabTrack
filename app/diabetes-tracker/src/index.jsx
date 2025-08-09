07bdcd1662eb20465c37ebc2fcaac2c52eb92de2
Abdulrahman Zahir <133116031+abdulrahmanzahir@users.noreply.github.com>
2025-08-09 18:11:32 +0300

    Update index.jsx

--- app/diabetes-tracker/src/index.jsx
+++ app/diabetes-tracker/src/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import DiabetesTrackerApp from './DiabetesTrackerApp'; // ✅ Correct import
+import DiabetesTrackerApp from './DiabetesTrackerApp';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <DiabetesTrackerApp /> {/* ✅ Correct component name */}
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/DiabTrack">
+      <DiabetesTrackerApp />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
